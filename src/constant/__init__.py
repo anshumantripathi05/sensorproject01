fba9327354a20ae9923c47f8c68017fb814e847c
Anshuman Tripathi <arpit.tripathi2569@gmail.com>
2026-06-02 13:17:02 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = 'water-fal=ult'
+MONGO_DATABASE_NAME = 'xyz'
+MONGO_COLLECTION_NAME = 'waterfault'
+
+
+TARGET_COLUMN = 'quality'
+MONGO_DB_URL = "mongodb=srv:"
+
+
+MODEL_FILE_NAME = 'model'
+MODEL_FILE_EXTENSION = '.pkl'
